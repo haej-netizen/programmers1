6fdac5f35054f3243e4485ef214be2074153a7e5
haej-netizen <gowjd1484@gmail.com>
2025-11-13 22:50:52 +0900

    [level 0] Title: 공 던지기, Time: 0.91 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.py
@@ -1,7 +1,7 @@
 def solution(numbers, k):
     answer = 0
-    numbers2 = numbers * k
+    numbers2 = numbers * k            # 리스트 numbers를 충분하게 반복
     
-    numbers_even = numbers2[0::2]
-    answer = numbers_even[k-1]
+    numbers_even = numbers2[0::2]     # 리스트 numbers 반복한 리스트 중에서 짝수번째만 추출
+    answer = numbers_even[k-1]        # 짝수번째만 추출한 리스트에서 k번째 숫자를 구함
     return answer
