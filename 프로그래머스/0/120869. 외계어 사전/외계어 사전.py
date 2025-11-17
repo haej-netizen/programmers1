1b6a23ea224e8a910e8538407153799d2c49e1f9
haej-netizen <gowjd1484@gmail.com>
2025-11-17 15:17:32 +0900

    [level 0] Title: 외계어 사전, Time: 0.02 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -0,0 +1,10 @@
+def solution(spell, dic):
+    answer = 2              # 기본값은 2 : 존재하지 않는 경우 
+    str_spell = "".join(spell)    # 리스트의 있는 문자를 문자열로 만들어줌
+    for i in range(0, len(dic)) : 
+        if sorted(str_spell) == sorted(dic[i]) :     # 정렬 후 비교
+            answer = 1
+            break          ## 찾았으면 바로 종료해줘야 함, 반복문이기 때문에 답이 계속 덮어씌여지기 때문에 끝내줘야함
+        
+    
+    return answer
