a3245f0e3e3d8d47dffa7b197b705b740fe55e93
haej-netizen <gowjd1484@gmail.com>
2025-11-24 14:31:17 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -3,9 +3,9 @@ def solution(num, total):
     
     first = 0
     
-    mul = (num) * (1+num)//2            # 1에 대한 등차수열의 합(등차수열의 합 공식 : n(2a+(n-1)d)/2
-    first = (total-mul)//num          # 답의 첫번째 숫자 구하기
+    mul = (num-1) * (num)//2            # 1에 대한 등차수열의 합,n-1까지(등차수열의 합 공식 : n(2a+(n-1)d)/2)
+    first = (total-mul)//num            # 답의 첫번째 숫자 구하기
     
-    for i in range(first+1, first+num+1): 
+    for i in range(first, first+num):   # 마지막 숫자는 first+num-1 이지만 범위이므로 조정함 
         answer.append(i)
     return answer
