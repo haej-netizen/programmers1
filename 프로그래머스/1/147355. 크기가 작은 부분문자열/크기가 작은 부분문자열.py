85fdaf1f3ff3625463586c7380ad56ad64b42d45
haej-netizen <gowjd1484@gmail.com>
2025-12-03 22:08:12 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 6.35 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -2,7 +2,7 @@ def solution(t, p):
     answer = 0
     num=0
     for i in range(len(t)-len(p)+1) : # 전체 문자열에서 p와 같은 길이의 숫자를 비교하고자 함
-        num = int(t[i:i+len(p)])      # 비교할 숫자는 전체 문자열 중 [시작하는지점 : 시작지점+len(p)]
+        num = int(t[i:i+len(p)])      # 비교할 숫자는 전체 문자열 중 [시작하는지점 : 시작지점+len(p)], len(p)+1 아님
         if int(p) >= num :
             answer += 1
         
