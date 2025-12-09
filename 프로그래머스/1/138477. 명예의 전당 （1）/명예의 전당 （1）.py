adf00e30df939ffd21a5ab1fd3ae6627b259f9da
haej-netizen <gowjd1484@gmail.com>
2025-12-09 17:40:45 +0900

    [level 1] Title: 명예의 전당 (1), Time: 5.77 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,9 +1,10 @@
 def solution(k, score):
     answer = []
     sco = []
+# 점수를 일단 새로운 리스트에 넣으면서 몇등까지만 구하고 그중에 최소값을 answer리스트에 계속추가함, score에 있는 개수만큼 반복 
     for i in range(len(score)) :
         sco.append(score[i])
-        sco = sorted(sco, reverse=True)[:k]
+        sco = sorted(sco, reverse=True)[:k]    #오름차순정렬 후 k번째까지만 반환(명예의 전당에 있는 점수만 반환)
         answer.append(min(sco))
         
     return answer
